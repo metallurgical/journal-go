3a03fec39b90e0325d284279c616023c92bf987c
Norlihazmey Ghazali <norlihazmey.ghazali@gmail.com>
2019-05-05 17:18:19 +0800

    adding rand token and random ref no

--- database/models/journal.go
+++ database/models/journal.go
@@ -1,15 +1,22 @@
 package models
 
 import (
+	"crypto/rand"
+	"encoding/base32"
 	"errors"
 	goslug "github.com/gosimple/slug"
 	"github.com/jinzhu/gorm"
 	pb "github.com/metallurgical/journal-go/api/golang"
+	intrand "math/rand"
+	"os"
+	"strconv"
+	"strings"
 	"time"
 )
 
 type Journal struct {
 	ID                uint `gorm:"primary_key"`
+	ReferenceNo       interface{}
 	UserId            uint64
 	JournalCategoryId uint64
 	Title             string
@@ -31,12 +38,26 @@ func (j *Journal) GetJournal(db gorm.DB, id int64) (error) {
 
 // Create new journal resource.
 func (j *Journal) CreateJournal(db gorm.DB, req *pb.JournalCreateRequest) error {
-	var slug string
+	var originalSlug, slug string
+	var count = 0
 
-	slug = goslug.Make(req.Title)
+	originalSlug = goslug.Make(req.Title)
+	db.Model(&Journal{}).Where("slug = ?", originalSlug).Count(&count)
+	if count > 0 {
+		strSlug := make(chan string)
+		go randToken(10, strSlug)
+		slug = <-strSlug
+		originalSlug = originalSlug + strings.ToLower(slug)
+	}
+
+	ref := make(chan int)
+	go randRef(db, ref)
+	refNo := <-ref
+
 	journal := &Journal{
+		ReferenceNo:       os.Getenv("JOURNAL_PREFIX_NO") + strconv.Itoa(refNo),
 		Title:             req.Title,
-		Slug:              slug,
+		Slug:              originalSlug,
 		Overview:          req.Overview,
 		UserId:            req.UserId,
 		JournalCategoryId: req.JournalCategoryId,
@@ -46,5 +67,31 @@ func (j *Journal) CreateJournal(db gorm.DB, req *pb.JournalCreateRequest) error 
 		return errors.New("Operation failed. Please try again.")
 	}
 	db.Create(&journal)
+
 	return nil
+}
+
+// Generate random string.
+func randToken(length int, strSlug chan<- string) {
+	randomBytes := make([]byte, 32)
+	_, err := rand.Read(randomBytes)
+	if err != nil {
+		panic(err)
+	}
+	strSlug <- base32.StdEncoding.EncodeToString(randomBytes)[:length]
+}
+
+// Generate random number.
+func randRef(db gorm.DB, ref chan<- int) {
+	intrand.Seed(time.Now().UnixNano())
+	max := 10000000000
+	min := 1111111111
+	refNo := min + intrand.Intn(max-min)
+	count := 0
+	db.Model(&Journal{}).Where("reference_no = ?", refNo).Count(&count)
+
+	if count > 0 {
+		randRef(db, ref)
+	}
+	ref <- refNo
 }
